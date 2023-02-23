f31c26a3ecc69fd8ac49f0f3c2590bbf397f61eb
LukaszKiwala <kiwala-lukasz@o2.pl>
2023-02-23 13:03:33 +0100

    pakiet

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,70 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+//1
+
+//  const galleryListEl = document.querySelector("ul");
+
+//  const elements = images.map(image => {
+  
+//   const galleryItemEl = document.createElement("li");
+//   galleryItemEl.classList.add("img-item");
+
+//   const galleryImgEl = document.createElement("img");
+//   galleryImgEl.src = image.url;
+//   galleryImgEl.alt = image.alt;
+
+//   galleryItemEl.appendChild(galleryImgEl);
+
+//   return galleryItemEl;
+
+// });
+
+// galleryListEl.append(...elements);
+
+
+//2
+
+// const galleryListEl = document.querySelector("ul");
+// const makeGallaryImg = galleryImg =>{
+//   return galleryImg.map(image => {
+  
+//   const galleryItemEl = document.createElement("li");
+//   galleryItemEl.classList.add("img-item");
+
+//   const galleryImgEl = document.createElement("img");
+//   galleryImgEl.src = image.url;
+//     galleryImgEl.alt = image.alt;
+//     galleryItemEl.appendChild(galleryImgEl);
+//     console.log(galleryItemEl);
+//   return galleryItemEl;
+
+// });
+
+// }
+
+// const elements = makeGallaryImg(images);
+// galleryListEl.append(...elements);
+
+
+//3
+const galleryListEl = document.querySelector("ul");
+const element = images
+  .map(function ({ url, alt }) {
+    return `<li><img src = '${url}' alt = '${alt}' width="320"></li>`;
+  })
+  .join("");
+galleryListEl.insertAdjacentHTML("afterbegin", element);
+
+document.body.style.margin = '0px';
+
+
+galleryListEl.style.cssText = `display: flex;
+  align-items: center;
+  justify-content: center;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  `;
+
